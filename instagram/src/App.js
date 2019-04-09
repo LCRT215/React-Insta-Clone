d731be8e0812cddad7d42b109523d24ac0687847
Leslie Thompson <lcrthompson@gmail.com>
2019-04-09 13:06:15 -0700

    day2

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import dummyData from "./dummy-data";
 import SearchBar from "./SearchBar/SearchBar";
 import "./App.css";
+import PostContainer from "./PostContainer/PostContainer";
 
 class App extends Component {
   constructor() {
@@ -13,10 +15,10 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBar />
+        <PostContainer posts={this.state.posts} />
       </div>
     );
   }
 }
 
 export default App;
-gi;
