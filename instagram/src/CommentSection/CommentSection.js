30f3cd380bd04a7a11619fd878a55f3629ae049e
Leslie Thompson <lcrthompson@gmail.com>
2019-04-09 15:58:17 -0700

    pause

--- instagram/src/CommentSection/CommentSection.js
+++ instagram/src/CommentSection/CommentSection.js
@@ -7,9 +7,11 @@ class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: props.comments
+      comments: props.comments,
+      comment: " "
     };
   }
+
   render() {
     return (
       <div>
--- instagram/src/SearchBar/SearchBar.js
+++ instagram/src/SearchBar/SearchBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./SearchBar.css";
+import logo from "../img/logo.png";
 
 const SearchBar = () => {
   return (
     <div className="search-bar-container">
       <div className="ig-img">
-        <img src="/img/logo.png" />
+        <img src={logo} alt="logo" />
       </div>
       <p>Instagram</p>
       <input type="text" name="search" placeholder="search" />
